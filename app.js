f7ba726683ef2604eb649393412650e312ed70bf
RamonWebDev <ramonplaysgames@gmail.com>
2025-01-28 18:57:07 -0600

    added funciton

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-api = 'https://api.tracker.gg/api/v2/marvel-rivals/standard/profile/ign/'; //api
+api = 'https://api.tracker.gg/api/v2/marvel-rivals/standard/profile/ign/eevee'; //api
+async function getData() {
+    const url = "https://api.tracker.gg/api/v2/marvel-rivals/standard/profile/ign/eevee";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  
